a7658588bfa422f6329f30f5dc10441feefe8443
Jonjo Twist <josh@molmorg.com>
2021-04-20 22:13:59 -0700

    using node module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Banana } from './dep';
+import ie from 'is-even';
 
 export default {
   async fetch(request: Request, env: any) {
     const b = new Banana();
-    return new Response(`hello from TS: ${b.color}`);
+    ie(3)
+    return new Response(`hello from TS: ${b.color}, ${ie(1)}, ${ie(2)}`);
   }
 }
